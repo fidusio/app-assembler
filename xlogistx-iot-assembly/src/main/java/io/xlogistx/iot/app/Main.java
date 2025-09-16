446eb3043c2d9a30318f08170fd7b21a9dbba881
javaconsigliere <javaconsigliere@gmail.com>
2025-09-15 19:59:25 -0700

    removed unnecessary dependencies changed parent to xlogistx-mvn

--- xlogistx-iot-assembly/src/main/java/io/xlogistx/iot/app/Main.java
+++ xlogistx-iot-assembly/src/main/java/io/xlogistx/iot/app/Main.java
@@ -18,13 +18,11 @@ import org.zoxweb.shared.util.ParamUtil;
 
 import java.io.File;
 
-public class Main
-{
+public class Main {
     private final static LogWrapper log = new LogWrapper(Main.class);
-    public static void main(String ...args)
-    {
-        try
-        {
+
+    public static void main(String... args) {
+        try {
 
             ParamUtil.ParamMap params = ParamUtil.parse("-", args);
             String wsConfig = params.stringValue("-wsc", true);
@@ -33,9 +31,7 @@ public class Main
             String i2cCommand = params.stringValue("-i2c", true);
 
 
-
-            if(wsConfig != null)
-            {
+            if (wsConfig != null) {
                 File file = IOUtil.locateFile(wsConfig);
                 HTTPServerConfig hsc = GSONUtil.fromJSON(IOUtil.inputStreamToString(file), HTTPServerConfig.class);
                 log.getLogger().info("HTTPServerConfig:" + hsc);
@@ -44,57 +40,47 @@ public class Main
                 httpServerCreator.setAppConfig(hsc);
                 httpServerCreator.createApp();
             }
-            if(flowConfig != null)
-            {
+            if (flowConfig != null) {
                 File file = IOUtil.locateFile(flowConfig);
                 String jsonFC = IOUtil.inputStreamToString(file);
-                log.getLogger().info("" + jsonFC);
+                log.getLogger().info(jsonFC);
                 PinStateMonitorConfig pinStateMonitorConfig = GSONUtil.fromJSONDefault(jsonFC, PinStateMonitorConfig.class);
                 new GPIOFlowProcessor(pinStateMonitorConfig, TaskUtil.defaultTaskScheduler()).init();
             }
-            if(cronConfig != null)
-            {
+            if (cronConfig != null) {
 
                 CronConfig cc = GSONUtil.fromJSON(IOUtil.inputStreamToString(cronConfig), CronConfig.class);
 
                 CronTool ct = new CronTool(TaskUtil.defaultTaskScheduler());
-                TaskUtil.defaultTaskScheduler().queue(cc.getSetupDelay(), ()->
+                TaskUtil.defaultTaskScheduler().queue(cc.getSetupDelay(), () ->
                 {
-                    try
-                    {
+                    try {
                         SunriseSunsetScheduler ssc = new SunriseSunsetScheduler(TaskUtil.defaultTaskProcessor(), null);
                         ssc.setAPIService(cc.getAPIEndpoint());
                         CronTask cronTask = ct.registerCronTask("day", ssc, ssc);
                         ct.registerCronTask("night", cronTask);
-                        for (CronSchedulerConfig scs : cc.getConfigs())
-                        {
+                        for (CronSchedulerConfig scs : cc.getConfigs()) {
                             ct.cron(scs);
                         }
-                    }
-                    catch (Exception e)
-                    {
+                    } catch (Exception e) {
                         error(e);
                     }
                 });
             }
-            if(i2cCommand != null)
-            {
+            if (i2cCommand != null) {
                 I2CUtil.exec(params);
             }
 
-            if(wsConfig == null && flowConfig == null && cronConfig == null && i2cCommand==null)
+            if (wsConfig == null && flowConfig == null && cronConfig == null && i2cCommand == null)
                 throw new IllegalArgumentException("No config found");
 
-        }
-        catch(Exception e)
-        {
-           error(e);
+        } catch (Exception e) {
+            error(e);
         }
     }
 
 
-    private static void error(Exception e)
-    {
+    private static void error(Exception e) {
         e.printStackTrace();
         System.err.println("command: [-wsc web-server-config.json] [-fc flow-config.json] [-cc cron_config.json] [-i2c [command] [command-params....]");
         I2CUtil.error("-i2c");
